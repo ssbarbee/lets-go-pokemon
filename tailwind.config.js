e7b1d51c321006f285fd460c009688ac889bbb9d
ssbarbee <ssbarbee@users.noreply.github.com>
2025-08-02 11:14:59 +0200

    fix: configure tailwind properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [
+        './src/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
