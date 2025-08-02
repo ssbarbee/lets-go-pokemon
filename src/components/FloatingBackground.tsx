577b628e532c656f34aa820a3b9d37d73c32c716
ssbarbee <ssbarbee@users.noreply.github.com>
2025-08-02 17:35:30 +0200

    fix: clicking the input

--- src/components/FloatingBackground.tsx
+++ src/components/FloatingBackground.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export const FloatingBackground = () => {
     return (
-        <div className="absolute inset-0 overflow-hidden z-0">
+        <div className="absolute inset-0 z-0 pointer-events-none">
             {floatingPokemons.map((poke, idx) => (
                 <Link href={`/pokemon/${poke.id}`} key={idx}>
                     <div
