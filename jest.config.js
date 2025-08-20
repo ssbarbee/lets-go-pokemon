f06c69be8d60952f97066363733c3ddfbaf49def
ssbarbee <ssbarbee@users.noreply.github.com>
2025-08-20 16:49:45 +0200

    fix: jest babel config preventing app from running

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1',
-    },
-    transform: {
-        '^.+\\.(ts|tsx)$': 'babel-jest',
-    },
-    testMatch: ['**/?(*.)+(test).[jt]s?(x)'],
-    transformIgnorePatterns: ['/node_modules/'],
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": ["babel-jest", { configFile: "./jest.babel.config.js" }],
+  },
+  testMatch: ["**/?(*.)+(test).[jt]s?(x)"],
+  transformIgnorePatterns: ["/node_modules/"],
 };
